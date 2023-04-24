d15fb438393ed2ea37ab7417c25d883b5a508014
Melodic Morenia <melodic@VDI-LNX-PLC-02.staff.ad.curtin.edu.au>
2023-04-25 00:52:37 +0800

    add param set cpp file

--- src/my_package/scripts/background.cpp
+++ src/my_package/scripts/background.cpp
@@ -1,19 +1,31 @@
 
 #include <ros/ros.h>
-#include <std_srvs/Empty.h>
+#include <ros/console.h>
+// #include <std_srvs/Empty.h>
 
-int main (int argc, char ∗∗ argv) {
-    ros::int ( argc, argv, " set_bg_color ");
+int main (int argc, char ** argv) {
+    // ros::int ( argc, argv, " set_bg_color ");
     ros::NodeHandle nh;
-    //This code will wait until a clear service is available. Once the program starts the background
-    will be reset.
+    //This code will wait until a clear service is available. Once the program starts the background will be reset.
     ros::service::waitForService ("clear");
+    // Get the maximum velocity parameter . 
+    const std :: string PARAM_NAME = "/turtlesim/background_r";
+    int background_col; 
+    bool ok = ros::param::get(PARAM_NAME, background_col);
+    if ( !ok ) { 
+        ROS_FATAL_STREAM( "Could not ge the parameter" << PARAM_NAME) ;
+        exit(1);
+    } else {
+        ROS_DEBUG("Background r: %d", background_col);
+    }
+
+    
     //Set the background colour and override the default colour.
     ros::param::set ("background_r",210);
     ros::param::set ("background_g",155);
     ros::param::set ("background_r",200);
-    // Get husky simulator to accept the new values of these parameter
-    ros::ServiceClient clearClient = nh.serviceClient <std_srvs::Empty>("/clear");
-    std_srvs::Empty srv;
-    clearClient.call(srv);
+
+    // ros::ServiceClient clearClient = nh.serviceClient <std_srvs::Empty>("/clear");
+    // std_srvs::Empty srv;
+    // clearClient.call(srv);
 }
